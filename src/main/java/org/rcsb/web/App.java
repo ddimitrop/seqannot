a0c9d23feee63fa333204807b32aef1a570e6712
Dimitris <ddimitrop@gmail.com>
2012-01-21 23:34:03 -0800

    first version

---
+++ src/main/java/org/rcsb/web/App.java
@@ -0,0 +1,13 @@
+package org.rcsb.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
